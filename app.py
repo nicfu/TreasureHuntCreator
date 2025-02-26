f9e065c13e8c813fc25c557465493966498fc35f
Nic Fouhy <nic.fouhy@gmail.com>
2025-02-27 01:26:44 +1300

    modified:   .gitignore 	new file:   __pycache__/auth.cpython-312.pyc 	new file:   __pycache__/models.cpython-312.pyc 	renamed:    app/app.py -> app.py 	deleted:    app/config.py 	deleted:    app/models.py 	new file:   config.py 	modified:   models.py 	new file:   requirements.txt 	renamed:    app/static/css/styles.css -> static/css/styles.css 	renamed:    app/templates/index.html -> templates/index.html

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, session
-from auth import auth_bp
-from auth import auth_bp
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from app.models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///treasure.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-app.register_blueprint(auth_bp)
 
 @app.route("/")
 def home():
